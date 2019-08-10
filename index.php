9eb3e4e0e1cb98b07ea33b4458ed2fe72ae3ed5f
codeatt <sergiojunior2000@hotmail.com>
2019-08-10 18:21:48 -0300

    profile pdf refresh

--- index.php
+++ index.php
@@ -1,11 +1,42 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+      integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+      crossorigin="anonymous"
+      >
+    <link rel = "stylesheet" type = "text/css" href = "css/style.css" />
     <title>Apresentação</title>
   </head>
   <body>
-    <h1>Olá Mundo</h1>
-    <h2><?php echo("Este é um código PHP") ?></h2>
+    <nav>
+      <a class="btn" href="#" id="home" style="color: yellow; float: left">Página em construção</a>
+			<a class="btn" href="index.html" id="home">Home</a>
+			<a class="btn" href="https://www.facebook.com/sergio.bentes.5">Facebook</a>
+			<a class="btn" href="https://github.com/codeatt">Github</a>
+			<a class="btn" href="https://www.linkedin.com/in/sergiobentes/">LinkedIn</a>
+      <a class="btn" href="#" id="home" style="color: yellow; float: right">Página em construção</a>
+		</nav>
+    <h1>Olá mundo, me chamo Sergio Bentes!</h1>
+    <p>Sou brasileiro e desenvolvedor fullstack vivendo em São Paulo capital. Fã de cinema, ciclismo e programação. Tenho interesse em viagens e jogos.<br><br> Clique no botão abaixo para baixar o currículo.</p>
+    <p hidden>Hey there, I’m Sergio. I’m a web developer living in São Paulo, Brasil. I am a fan of movies, cycling, and programming. I’m also interested in travel and gaming. You can hire me with a click on the button above.</p>
+  <div class="btn-group" role="group">
+    <ul>
+      <!-- <li><a href="home.html">Home</a></li>
+      <li><a href="skill.html">Skills</a></li>
+      <li><a href="portfolio.html">Portfolio</a></li>
+      <li><a href="contato.html">Contato</a></li> -->
+      <li><a
+        type="button" class="btn"
+        href="pdf/profile.pdf"
+        title="clique para baixar">Curriculo PDF</a>
+      </li>
+    </ul>
+  </div>
   </body>
 </html>
